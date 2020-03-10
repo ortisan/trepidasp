e0fc49d4b5fc8c1f0427d62b2601980784ec2f29
Jason Pires <jason.pires@outlook.com>
2020-03-10 20:46:41 -0300

    Mudança do código da MainActivity para HomeFragment Ajuste para exibir a localização no campo de texto Ajuste no evento do sensor para ativar localização realtime

--- app/src/main/java/br/com/jsonthegroup/trepidasp/MainActivity.java
+++ app/src/main/java/br/com/jsonthegroup/trepidasp/MainActivity.java
@@ -1,62 +1,19 @@
 package br.com.jsonthegroup.trepidasp;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.drm.DrmStore;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Looper;
-import android.provider.Settings;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.android.gms.common.internal.Constants;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import java.util.List;
-import java.util.Locale;
-
 public class MainActivity extends AppCompatActivity {
-
-    int PERMISSION_ID = 44;
-    FusedLocationProviderClient mFusedLocationClient;
-    private LocationCallback mLocationCallback = new LocationCallback(){
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            Location mLastLocation = locationResult.getLastLocation();
-            mLastLocation.getLatitude();
-            mLastLocation.getLongitude();
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        getLastLocation();
 
         //BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -69,110 +26,4 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         //NavigationUI.setupWithNavController(navView, navController);
     }
-
-
-    @Override
-    public void onResume(){
-        super.onResume();
-        if (checkPermissions()) {
-            getLastLocation();
-        }
-    }
-
-    private boolean checkPermissions(){
-        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestPermissions(){
-        ActivityCompat.requestPermissions(
-                this,
-                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
-                PERMISSION_ID
-        );
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_ID) {
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getLastLocation();
-            }
-        }
-    }
-
-    private boolean isLocationEnabled(){
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-                LocationManager.NETWORK_PROVIDER
-        );
-    }
-
-    private void getLastLocation(){
-        if(checkPermissions()){
-            if(isLocationEnabled()){
-                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        Location location = task.getResult();
-                        if (location == null){
-                            requestNewLocationData();
-                        } else {
-                            location.getLatitude();
-                            location.getLongitude();
-                            getCurrentAddress(location);
-                        }
-                    }
-                });
-            } else {
-                Toast.makeText(this, "Por favor ative a Localização", Toast.LENGTH_LONG);
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        } else {
-            requestPermissions();
-        }
-
-    }
-
-    private void requestNewLocationData() {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(0);
-        mLocationRequest.setFastestInterval(0);
-        mLocationRequest.setNumUpdates(1);
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
-    }
-
-    private void getCurrentAddress(Location location){
-        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
-        List<Address> addresses;
-        try {
-            addresses = gcd.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-            if (addresses.size() > 0) {
-                String address = addresses.get(0).getAddressLine(0);
-                String locality = addresses.get(0).getLocality();
-                String subLocality = addresses.get(0).getSubLocality();
-                String state = addresses.get(0).getAdminArea();
-                String country = addresses.get(0).getCountryName();
-                String postalCode = addresses.get(0).getPostalCode();
-                String knownName = addresses.get(0).getFeatureName();
-                if (subLocality != null) {
-                    // locality + "," + subLocality;
-                } else {
-                    // locality;
-                }
-                // locality;
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Ocorreu um erro ao obter a localozação", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
 }
--- app/src/main/java/br/com/jsonthegroup/trepidasp/ui/home/HomeFragment.java
+++ app/src/main/java/br/com/jsonthegroup/trepidasp/ui/home/HomeFragment.java
@@ -2,28 +2,46 @@ package br.com.jsonthegroup.trepidasp.ui.home;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
+import java.util.List;
+import java.util.Locale;
 
 import br.com.jsonthegroup.trepidasp.R;
 
@@ -31,7 +49,6 @@ public class HomeFragment extends Fragment implements SensorEventListener {
 
     private HomeViewModel homeViewModel;
 
-    private LocationManager locationManager;
     private SensorManager sensorManager;
     private Sensor sensor;
 
@@ -49,6 +66,10 @@ public class HomeFragment extends Fragment implements SensorEventListener {
     private OutputStreamWriter reportGiroscopio;
     private OutputStreamWriter reportAcelerometro;
 
+    int PERMISSION_ID = 44;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private TextView textLocalizacao2;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -65,8 +86,6 @@ public class HomeFragment extends Fragment implements SensorEventListener {
 
         textAcelerometro2 = root.findViewById(R.id.text_acelerometro2);
         textGiroscopio2 = root.findViewById(R.id.text_giroscopio2);
-
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
         sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
 
@@ -100,17 +119,19 @@ public class HomeFragment extends Fragment implements SensorEventListener {
             }
         });
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
+        textLocalizacao2 = root.findViewById(R.id.text_localizacao2);
+        getLastLocation();
+
         return root;
     }
 
     @Override
     public void onResume() {
-
-
         super.onResume();
-//        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            throw new SecurityException("Faltou permissão de gps");
-//        }
+        if (checkPermissions()) {
+            getLastLocation();
+        }
     }
 
     @Override
@@ -129,8 +150,6 @@ public class HomeFragment extends Fragment implements SensorEventListener {
     }
 
     private void startFiles(String tipo) {
-
-
         try {
             File folderMyApp = new File(Environment.getExternalStorageDirectory() + "/" + "TrepidaSp/");
             if (!folderMyApp.exists()) {
@@ -205,10 +224,116 @@ public class HomeFragment extends Fragment implements SensorEventListener {
                 throw new RuntimeException("Erro");
             }
         }
+
+        getLastLocation();
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+
+
+    private void getLastLocation(){
+        if(checkPermissions()){
+            if(isLocationEnabled()){
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null){
+                            requestNewLocationData();
+                        } else {
+                            getCurrentAddress(location);
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this.getActivity(), "Por favor ative a Localização!", Toast.LENGTH_LONG);
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            requestPermissions();
+        }
+    }
+
+    private void requestPermissions(){
+        ActivityCompat.requestPermissions(
+                this.getActivity(),
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_ID
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_ID) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation();
+            }
+        }
+    }
+
+    private boolean checkPermissions(){
+        return ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean isLocationEnabled(){
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        );
+    }
+
+    private void requestNewLocationData() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            mLastLocation.getLatitude();
+            mLastLocation.getLongitude();
+            getCurrentAddress(mLastLocation);
+        }
+    };
+
+    private void getCurrentAddress(Location location){
+        Geocoder gcd = new Geocoder(this.getContext(), Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = gcd.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (addresses.size() > 0) {
+                String address = this.handleNullEmptyString(addresses.get(0).getAddressLine(0));
+                String locality = this.handleNullEmptyString(addresses.get(0).getLocality());
+                String subLocality = this.handleNullEmptyString(addresses.get(0).getSubLocality());
+                String state = this.handleNullEmptyString(addresses.get(0).getAdminArea());
+                String country = this.handleNullEmptyString(addresses.get(0).getCountryName());
+                String postalCode = this.handleNullEmptyString(addresses.get(0).getPostalCode());
+                String knownName = this.handleNullEmptyString(addresses.get(0).getFeatureName());
+
+                String completeAddress = address + ", " + locality + ", " + subLocality + ", " + subLocality + ", " + state + ", " + country + ", " + postalCode + ", " + knownName;
+                textLocalizacao2.setText(completeAddress);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this.getActivity(), "Ocorreu um erro ao obter a localização", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String handleNullEmptyString(String s){
+        return s == null ? "" : s.trim().toUpperCase();
     }
 }
